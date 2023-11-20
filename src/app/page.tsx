746d7675c476f2deeecdaac1c644c925168af239
thoulee <thou_lee@outlook.com>
2023-11-20 13:58:38 +0800

    add pokemon search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,75 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from 'react';
+import { Container, FloatingLabel, Form, ListGroup, Navbar } from 'react-bootstrap';
+import './page.module.css';
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+interface Pokemon {
+  name: string;
+  url: string;
 }
+
+function Page() {
+  const [datas, setDatas] = useState([] as Array<Pokemon>);
+  const [showDatas, setShowDatas] = useState([] as Array<Pokemon>);
+
+  const getData = () => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/`)
+      .then((response) => response.json())
+      .then((data) => {
+        setDatas(data.results)
+        setShowDatas(data.results)
+      });
+  }
+
+  const onSearch = (e: any) => {
+    const value = e.target.value;
+    let filtered = [];
+    if (value !== '') {
+      filtered = datas.filter((item) => {
+        return item.name.includes(value);
+      });
+      setShowDatas(filtered);
+    } else {
+      setShowDatas(datas);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <Navbar variant='dark' bg='dark'>
+        <Container>
+          <Navbar.Brand>Pokemon</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Container>
+        <FloatingLabel label='Search'>
+          <Form.Control
+            type='text'
+            id='scerchText'
+            onChange={onSearch}
+            placeholder='Search'
+          />
+        </FloatingLabel>
+        <ListGroup >
+          {showDatas.map((item) => {
+            return (
+              <ListGroup.Item
+                key={item.url}
+              >
+                {item.name}
+              </ListGroup.Item>
+            )
+          })}
+        </ListGroup>
+      </Container>
+    </div>
+  );
+}
+
+export default Page;
