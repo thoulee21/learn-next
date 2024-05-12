f7b58a65c5cd9fac97014fa8b78aeea373a76b50
thoulee <thou_lee@outlook.com>
2024-05-12 15:49:38 +0800

    fix next config module

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     /**
-    * Enable static exports for the App Router.
-    *
-    * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-    */
+     * Enable static exports for the App Router.
+     *
+     * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+     */
     output: "export",
 
     /**
-     * Set base path. This is the slug of your GitHub repository.
+     * Set base path. This is usually the slug of your repository.
      *
      * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
      */
@@ -18,11 +18,11 @@ const nextConfig = {
      * Disable server-based image optimization. Next.js does not support
      * dynamic features with static exports.
      *
-     * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+     * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
      */
     images: {
         unoptimized: true,
     },
-}
+};
 
-module.exports = nextConfig
+export default nextConfig;
